1151623c44638bbca1fb6817019e201bba03f0ea
WONI2 <126743165+WONI2@users.noreply.github.com>
2023-06-09 17:51:11 +0900

    signup config

--- src/main/java/com/example/todo/config/CorsConfig.java
+++ src/main/java/com/example/todo/config/CorsConfig.java
@@ -9,6 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
